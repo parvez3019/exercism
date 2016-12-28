778b48887e76922f7a9cf26ea7afac4298e673b8
parvez <pha3019@gmail.com>
2016-12-28 14:40:58 +0530

    Hello world exercise

---
+++ java/hello-world/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+  public static String hello(String name) {
+    if (name == "Alice") {
+      return "Hello, Alice!";
+    }
+    if (name == "Bob"){
+      return "Hello, Bob!";
+    }
+    return "Hello, World!";
+  }
+}
