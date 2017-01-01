5a4a42a3e509c42bdab29fa75c8a081b247bf0a9
parvez <pha3019@gmail.com>
2017-01-01 18:01:23 +0530

    Anagram exercise added

---
+++ java/anagram/src/main/java/Anagram.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Anagram {
+  private String string;
+  private ArrayList<String> list;
+
+  public Anagram(String string) {
+    this.string = string;
+    list = new ArrayList<>();
+  }
+
+
+  public List<String> match(List<String> strings) {
+    for (String s: strings) {
+      if(!s.equalsIgnoreCase(string) && isAnagram(s.toLowerCase(),string.toLowerCase())){
+        list.add(s);
+      }
+    }
+    return list;
+  }
+
+  public boolean isAnagram(String firstWord, String secondWord) {
+    char[] word1 = firstWord.replaceAll("[\\s]", "").toCharArray();
+    char[] word2 = secondWord.replaceAll("[\\s]", "").toCharArray();
+    Arrays.sort(word1);
+    Arrays.sort(word2);
+    return Arrays.equals(word1, word2);
+  }
+
+  public boolean isEmpty() {
+    return list.size() == 0;
+  }
+}
